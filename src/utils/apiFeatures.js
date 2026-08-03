f80e7bbccaad8a8b1d4a151dea275d3d895e13bb
Khuram Mirza <mirzakhuram101@gmail.com>
2026-08-03 16:38:21 +0500

    feat: create APIFeatures class for filtering, sorting, limiting, and pagination

---
+++ src/utils/apiFeatures.js
@@ -0,0 +1,45 @@
+export class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObject = { ...this.queryString };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((field) => delete queryObject[field]);
+
+    let queryStr = JSON.stringify(queryObject);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.replaceAll(",", " ");
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("-createdAt");
+    }
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.replaceAll(",", " ");
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select("-__v");
+    }
+    return this;
+  }
+
+  paginate() {
+    const page = Number.parseInt(this.queryString.page, 10) || 1;
+    const limit = Number.parseInt(this.queryString.limit, 10) || 10;
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
+}
