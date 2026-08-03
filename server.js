cd776409f3416efb4941e011b93c14715b1ccb63
Khuram Mirza <mirzakhuram101@gmail.com>
2026-08-03 15:01:53 +0500

    config: install mongoose and connect to mongoDB cloud.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import app from "./src/app.js";
+import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 8000;
 
+await connectDB();
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
 });
