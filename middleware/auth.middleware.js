205518d9d701f648dea687070f362f9dfadead8a
Khuram Mirza <mirzakhuram101@gmail.com>
2026-08-11 19:22:39 +0500

    feat: auth middleware now get and verifies jwt from the res.cookie instead of auth headers

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -2,15 +2,11 @@ import { AppError } from "../src/utils/AppError.js";
 import jwt from "jsonwebtoken";
 
 export default async function verifyToken(req, res, next) {
-  let token;
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  )
-    token = req.headers.authorization.split(" ")[1];
+  const token = req.cookies?.token ?? null;
+  if (!token) {
+    throw new AppError("Not authorized to access this route", 401);
+  }
 
-  if (!token)
-    return next(new AppError("Not authorized access this route", 401));
   try {
     req.user = jwt.verify(token, process.env.JWT_SECRET);
     next();
