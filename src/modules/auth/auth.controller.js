17c5759b2c0f96fb7c7eb6fb4fdf17c180fcd9df
Khuram Mirza <mirzakhuram101@gmail.com>
2026-08-11 19:24:05 +0500

    fix: login and register functions now set jew response cookie instead of sending token with the response.

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -3,12 +3,18 @@ import * as authService from "./auth.service.js";
 export const register = async (req, res) => {
   const result = await authService.register(req.body);
 
+  res.cookie("token", result.token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 24 * 60 * 60 * 1000,
+  });
+
   return res.status(201).json({
     success: true,
     message: "User registered successfully",
     data: {
       user: result.user,
-      token: result.token,
     },
   });
 };
@@ -17,12 +23,18 @@ export const login = async (req, res) => {
   const { email, password } = req.body;
   const result = await authService.login(email, password);
 
+  res.cookie("token", result.token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 24 * 60 * 60 * 1000,
+  });
+
   return res.status(201).json({
     success: true,
     message: "Logged in successfully",
     data: {
       user: result.user,
-      token: result.token,
     },
   });
 };
